151d0d0052c8bc0f92765a7b08d5b51da325f573
Ronak-Razavi <132320940+Ronak-Razavi@users.noreply.github.com>
2023-05-30 10:39:07 -0400

    Strlen function use

---
+++ StrlenUserNameLength.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char name[50];
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    int length = strlen(name);
+    printf("Name: %s\n", name);
+    printf("Length of the name: %d\n", length);
+
+    printf("Array representation of the name:\n");
+    for (int i = 0; i < length; i++) {
+        printf("%c ", name[i]);
+    }
+    printf("\n");
+
+    printf("Location of each element in the array:\n");
+    for (int i = 0; i < length; i++) {
+        printf("%c: %p\n", name[i], &name[i]);
+    }
+
+    return 0;
+}
