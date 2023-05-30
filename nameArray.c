0ed2c670eca82de8bb090e88bdfdcb7b32979f58
Ronak-Razavi <132320940+Ronak-Razavi@users.noreply.github.com>
2023-05-30 10:41:21 -0400

    create Array of your Name

---
+++ nameArray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char d[6];
+    d[0]='R';
+    d[1]='o';
+    d[2]='n';
+    d[3]='a';
+    d[4]='k';
+    d[5]='\0';
+printf("%s\n",d);
+
+    return 0;
+}
